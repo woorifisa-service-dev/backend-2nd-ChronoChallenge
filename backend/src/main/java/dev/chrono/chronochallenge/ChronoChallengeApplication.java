2c0c32e3dfcf4be8f85eb1dae87a11e904ab0fc7
박지윤 <jyun1937@naver.com>
2023-07-11 15:22:44 +0900

    chore: project setup

---
+++ backend/src/main/java/dev/chrono/chronochallenge/ChronoChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.chrono.chronochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/chrono/chronochallenge/ChronoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chrono.chronochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
