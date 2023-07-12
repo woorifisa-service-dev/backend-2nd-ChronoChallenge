58bb3317af7dc5989f11420dc88216fd0c977f33
juneheel <131724311+juneheel@users.noreply.github.com>
2023-07-12 10:56:50 +0900

    refactor: Member Entity 수정, Column 조건추가

--- backend/src/main/java/dev/chrono/chronochallenge/model/Member.java
+++ backend/src/main/java/dev/chrono/chronochallenge/model/Member.java
@@ -3,6 +3,7 @@ package dev.chrono.chronochallenge.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -17,19 +18,22 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(length = 20,nullable = false)
     private String name;
 
-    @Column
+    @Column(length = 20,nullable = false)
     private int password;
 
-    @Column
+    @Column(nullable = false)
+    @ColumnDefault("false")
     private boolean authority;
 
     @Column
+    @ColumnDefault("0")
     private int point;
 
     @Column(name = "pass_ticket")
+    @ColumnDefault("0")
     private int passTicket;
 
     @Builder
