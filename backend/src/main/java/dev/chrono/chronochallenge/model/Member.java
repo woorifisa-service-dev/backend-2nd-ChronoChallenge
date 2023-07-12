613e0d2dc04031f3266ef8b17e84ce518aa6d3a3
JUNI <131724311+juneheel@users.noreply.github.com>
2023-07-12 09:22:06 +0900

    feat: Quiz Entity 추가 (#13)

--- backend/src/main/java/dev/chrono/chronochallenge/model/Member.java
+++ backend/src/main/java/dev/chrono/chronochallenge/model/Member.java
@@ -17,49 +17,29 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="correct_person")
-    private Long correctPerson;
+    @Column
+    private String name;
 
     @Column
-    private int answer;
+    private int password;
 
-    @Column(name="multiple_candidate1")
-    private String multipleCandidate1;
-
-    @Column(name="multiple_candidate2")
-    private String multipleCandidate2;
-
-    @Column(name="multiple_candidate3")
-    private String multipleCandidate3;
-
-    @Column(name="multiple_candidate4")
-    private String multipleCandidate4;
+    @Column
+    private boolean authority;
 
     @Column
     private int point;
 
-    @Column
-    private boolean status;
-
-    @Column(name="createdAt")
-    private Date created_at;
-
-    @Column(name="modifiedAt")
-    private Date modified_at;
+    @Column(name = "pass_ticket")
+    private int passTicket;
 
     @Builder
-    public Member(Long id, Long correctPerson, int answer, String multipleCandidate1, String multipleCandidate2, String multipleCandidate3, String multipleCandidate4, int point, boolean status, Date created_at, Date modified_at) {
+    public Member(Long id, String name, int password, boolean authority, int point, int passTicket) {
         this.id = id;
-        this.correctPerson = correctPerson;
-        this.answer = answer;
-        this.multipleCandidate1 = multipleCandidate1;
-        this.multipleCandidate2 = multipleCandidate2;
-        this.multipleCandidate3 = multipleCandidate3;
-        this.multipleCandidate4 = multipleCandidate4;
+        this.name = name;
+        this.password = password;
+        this.authority = authority;
         this.point = point;
-        this.status = status;
-        this.created_at = created_at;
-        this.modified_at = modified_at;
+        this.passTicket = passTicket;
     }
 }
 
