44cd2a0d76dc373b34d6e172129c364c2181f3f4
JIYUN <72537762+Jiyun-Parkk@users.noreply.github.com>
2023-07-12 11:55:50 +0900

    Refactor/modify project structure (#23)
    * refacotr: 기능별 mvc구조로 변경
    
    * refactor: crudservice common으로 이동
    
    ---------
    
    Co-authored-by: Junyong Moon <jymoon1121@gmail.com>

--- backend/src/main/java/dev/chrono/chronochallenge/member/service/MemberService.java
+++ backend/src/main/java/dev/chrono/chronochallenge/member/service/MemberService.java
@@ -1,6 +1,6 @@
 package dev.chrono.chronochallenge.member.service;
 
-import dev.chrono.chronochallenge.member.model.Member;
+import dev.chrono.chronochallenge.common.service.CrudSevice;
 
 public interface MemberService extends CrudSevice<Member, Long> {
 
