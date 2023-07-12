958fd3ccff9eb94af827496a5794d23d11180300
TAESIC KONG <81614820+xotlr333@users.noreply.github.com>
2023-07-12 18:00:00 +0900

    Feat: Swagger 구현 (#29)
    * feat: Swagger 의존성 추가 및 설정 파일 생성
    
    * feat: quiz API에 Swagger 적용
    
    @ApiParam, @ApiOperation을 이용해서 Swagger의 추가 설명을 적용했다.

--- backend/src/main/java/dev/chrono/chronochallenge/quiz/controller/QuizController.java
+++ backend/src/main/java/dev/chrono/chronochallenge/quiz/controller/QuizController.java
@@ -4,6 +4,8 @@ import dev.chrono.chronochallenge.quiz.dto.response.QuizResponse;
 import dev.chrono.chronochallenge.quiz.dto.rquest.QuizRequest;
 import dev.chrono.chronochallenge.quiz.model.Quiz;
 import dev.chrono.chronochallenge.quiz.service.QuizService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,7 @@ public class QuizController {
         this.quizService = quizService;
     }
 
+    @ApiOperation(value = "퀴즈 전체 목록 조회")
     @GetMapping
     public List<QuizResponse> getQuizList(){
         return quizService.findAll()
@@ -31,23 +34,33 @@ public class QuizController {
                 .collect(Collectors.toList());
     }
 
+    @ApiOperation(value = "퀴즈 조회")
     @GetMapping("/{id}")
-    public QuizResponse getQuiz(@PathVariable Long id){
+    public QuizResponse getQuiz(
+            @ApiParam(value = "퀴즈 아이디", required = true, example = "1") @PathVariable Long id
+    ){
         Quiz quiz = quizService.findById(id);
         return QuizResponse.From(quiz);
     }
 
+    @ApiOperation(value = "퀴즈 추가")
     @PostMapping("/add")
-    public ResponseEntity<QuizResponse> saveQuiz(@RequestBody QuizRequest quizRequest){
+    public ResponseEntity<QuizResponse> saveQuiz(
+            @ApiParam(value = "추가할 퀴즈 정보", required = true, example = "1") @RequestBody QuizRequest quizRequest
+    ){
         Quiz quiz = QuizRequest.toQuiz(quizRequest);
         Quiz saveQuiz = quizService.save(quiz);
         QuizResponse quizResponse = QuizResponse.From(saveQuiz);
         return ResponseEntity.status(HttpStatus.OK).body(quizResponse);
     }
 
+    @ApiOperation(value = "퀴즈 수정")
     @Transactional
     @PutMapping("/{id}")
-    public ResponseEntity<QuizResponse> updateQuiz(@PathVariable Long id, @RequestBody QuizRequest quizRequest){
+    public ResponseEntity<QuizResponse> updateQuiz(
+            @ApiParam(value = "퀴즈 아이디", required = true, example = "1") @PathVariable Long id,
+            @ApiParam(value = "수정할 퀴즈 정보", required = true, example = "1") @RequestBody QuizRequest quizRequest
+    ){
         Quiz quiz = quizService.findById(id);
         quiz.update(quizRequest);
         QuizResponse quizResponse = QuizResponse.From(quiz);
@@ -55,8 +68,11 @@ public class QuizController {
         return ResponseEntity.status(HttpStatus.OK).body(quizResponse);
     }
 
+    @ApiOperation(value = "퀴즈 삭제")
     @DeleteMapping("/{id}")
-    public ResponseEntity<QuizResponse> deleteQuiz(@PathVariable Long id){
+    public ResponseEntity<QuizResponse> deleteQuiz(
+            @ApiParam(value = "퀴즈 아이디", required = true, example = "1") @PathVariable Long id
+    ){
         QuizResponse quiz = QuizResponse.From(quizService.findById(id));
         quizService.deleteById(id);
 
