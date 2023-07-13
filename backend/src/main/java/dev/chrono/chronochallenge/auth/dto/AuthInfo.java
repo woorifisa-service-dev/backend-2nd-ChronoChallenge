f39b20737433d4313d6b78c582ad39dfefa0d2f1
leejunehee <junehee107@naver.com>
2023-07-13 16:41:49 +0900

    refactor : Auth 로그인시 전달 데이터 추가

--- backend/src/main/java/dev/chrono/chronochallenge/auth/dto/AuthInfo.java
+++ backend/src/main/java/dev/chrono/chronochallenge/auth/dto/AuthInfo.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 public class AuthInfo {
     private Long id;
     private String name;
+    private boolean authority;
 
-    public AuthInfo(Long id, String name) {
+    public AuthInfo(Long id, String name,boolean authority) {
         this.id = id;
         this.name = name;
+        this.authority =authority;
     }
 }
