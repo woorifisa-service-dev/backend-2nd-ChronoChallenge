37f6e333e8fa019c762940c875c82336a110b3ca
JIYUN <72537762+Jiyun-Parkk@users.noreply.github.com>
2023-07-13 16:54:56 +0900

    feat: 전체 ui 구현 (#46)

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -1,13 +1,57 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      <router-link to="/">
+        <v-icon icon="mdi-circle-slice-6" />
+        Late Pass
+      </router-link>
     </v-app-bar-title>
+    <div class="user-box" v-if="userStore.user.name">
+      <span> {{ userStore.user.name }} 님 </span>
+      <v-btn
+        icon="mdi-account"
+        size="x-small"
+        @click="handleClickAccount"
+      ></v-btn>
+      <v-btn outlined @click="handleClickLogout"> Logout </v-btn>
+    </div>
+    <div class="user-box" v-else>
+      <v-btn outlined color="primary">
+        <router-link to="/login">Login</router-link>
+      </v-btn>
+    </div>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+import router from "@/router";
+import { useUserStore } from "@/store/useUserStore";
+import axios from "axios";
+import { ref } from "vue";
+
+const userStore = useUserStore();
+const isTeacher = ref(false);
+
+const handleClickAccount = () => {
+  isTeacher ? router.push("/admin") : router.push("/mypage");
+};
+
+const handleClickLogout = async () => {
+  try {
+    await axios({
+      url: "http://localhost:8080/auth/logout",
+      method: "post",
+    });
+    userStore.resetUser();
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
+
+<style scoped>
+.user-box {
+  display: flex;
+  align-items: center;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,60 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home";
+import Login from "@/views/Login";
+import MyPage from "@/views/MyPage";
+import Admin from "@/views/Admin";
+import QuizList from "@/views/QuizList";
+import Quiz from "@/views/Quiz";
+import CreateAndEditQuiz from "@/views/createAndEditQuiz";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        path: "",
+        name: "Home",
+        component: Home,
+      },
+      {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "/mypage",
+        name: "mypage",
+        component: MyPage,
+      },
+      {
+        path: "/admin",
+        name: "admin",
+        component: Admin,
+      },
+      {
+        path: "/quizList",
+        name: "quizList",
+        component: QuizList,
+      },
+      {
+        path: "/quiz",
+        name: "quiz",
+        component: Quiz,
+      },
+      {
+        path: "/createAndEditQuiz",
+        name: "createAndEditQuiz",
+        component: CreateAndEditQuiz,
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
