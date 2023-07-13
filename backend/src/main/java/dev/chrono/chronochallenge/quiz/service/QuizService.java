8bc3368972d55e29e40abdaf166da4b172d5dc77
TAESIC KONG <81614820+xotlr333@users.noreply.github.com>
2023-07-13 15:43:34 +0900

    fix: QuizService 수정 (#42)
    * Merge branch 'main' of /Users/gongtaesig/Desktop/kong/springboot project/backend-2nd-ChronoChallenge with conflicts.
    
    * fix: QuizService 수정
    
    QuizService 파일에 
    List<Quiz> findByStatus(QuizStatus quizStatus)
    추가했다.
    
    * fix: application.propertice 수정

--- backend/src/main/java/dev/chrono/chronochallenge/quiz/service/QuizService.java
+++ backend/src/main/java/dev/chrono/chronochallenge/quiz/service/QuizService.java
@@ -5,6 +5,10 @@ import dev.chrono.chronochallenge.member.model.Member;
 import dev.chrono.chronochallenge.quiz.model.Quiz;
 import dev.chrono.chronochallenge.quiz.model.QuizStatus;
 
+import java.util.List;
+
 public interface QuizService extends CrudService<Quiz, Long> {
     void updateStatusAndAnswerMember(Quiz quiz, QuizStatus quizStatus, Member answerMember);
+
+    List<Quiz> findByStatus(QuizStatus quizStatus);
 }
