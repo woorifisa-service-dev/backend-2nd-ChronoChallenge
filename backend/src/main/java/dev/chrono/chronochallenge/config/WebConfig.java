0307a134567cccf22160190606ff0de4534b0678
박지윤 <jyun1937@naver.com>
2023-07-28 18:26:15 +0900

    refactor: cors origin url 로컬 주소 추가

--- backend/src/main/java/dev/chrono/chronochallenge/config/WebConfig.java
+++ backend/src/main/java/dev/chrono/chronochallenge/config/WebConfig.java
@@ -7,7 +7,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://52.78.250.225") // 허용할 출처
+                .allowedOrigins("http://52.78.250.225","http://localhost:3000","http://127.0.0.1:3000") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
